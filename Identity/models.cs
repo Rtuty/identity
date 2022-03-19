750bdd58099ba31d9e103ffe0ce1f0aed7b48277
User3 <denisromanov020604@gmail.com>
2022-03-19 13:04:09 +0300

    model.cs + classes

--- Identity/models.cs
+++ Identity/models.cs
@@ -1,10 +1,24 @@
-﻿public class User
+﻿namespace Identity;
+public class User
 {
     public int Id { get; set; }
     public string Login { get; set; }
     public string Password { get; set; }
+    public Role Role { get; set; } = Role.RegularUser;
 }
 public enum Role
 {
     Admin, RegularUser, PrivilegedUser, Moderator
+}
+
+public class RegisterBody
+{
+    public string Login { get; set; }
+    public string Password { get; set; }
+}
+
+public class AuthorizationBody
+{
+    public string Login { get; set; }
+    public string Password { get; set; }
 }
